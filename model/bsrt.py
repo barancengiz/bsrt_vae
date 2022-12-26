ad7d7869bb042ce955ce336e0eb22bc5c1652246
Baran Cengiz <baran.cengiz@metu.edu.tr>
2022-12-26 23:04:19 +0300

    Implement VAE for reconstruction

--- model/bsrt.py
+++ model/bsrt.py
@@ -282,7 +282,175 @@ class CrossNonLocal_Fusion(nn.Module):
 
         return fea
 
+class ResizeConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, scale_factor, mode='nearest'):
+        super().__init__()
+        self.scale_factor = scale_factor
+        self.mode = mode
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=1)
 
+    def forward(self, x):
+        x = F.interpolate(x, scale_factor=self.scale_factor, mode=self.mode)
+        x = self.conv(x)
+        return x
+
+class BasicBlockEnc(nn.Module):
+
+    def __init__(self, in_planes, stride=1):
+        super().__init__()
+
+        planes = in_planes*stride
+
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        if stride == 1:
+            self.shortcut = nn.Sequential()
+        else:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes)
+            )
+
+    def forward(self, x):
+        out = torch.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = torch.relu(out)
+        return out
+
+class BasicBlockDec(nn.Module):
+
+    def __init__(self, in_planes, stride=1):
+        super().__init__()
+
+        planes = int(in_planes/stride)
+
+        self.conv2 = nn.Conv2d(in_planes, in_planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(in_planes)
+        # self.bn1 could have been placed here, but that messes up the order of the layers when printing the class
+
+        if stride == 1:
+            self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+            self.bn1 = nn.BatchNorm2d(planes)
+            self.shortcut = nn.Sequential()
+        else:
+            self.conv1 = ResizeConv2d(in_planes, planes, kernel_size=3, scale_factor=stride)
+            self.bn1 = nn.BatchNorm2d(planes)
+            self.shortcut = nn.Sequential(
+                ResizeConv2d(in_planes, planes, kernel_size=3, scale_factor=stride),
+                nn.BatchNorm2d(planes)
+            )
+
+    def forward(self, x):
+        out = torch.relu(self.bn2(self.conv2(x)))
+        out = self.bn1(self.conv1(out))
+        out += self.shortcut(x)
+        out = torch.relu(out)
+        return out
+
+class ResNet18Enc(nn.Module):
+
+    def __init__(self, num_Blocks=[2,2,2,2], z_dim=10, nc=3):
+        super().__init__()
+        self.in_planes = 64
+        self.z_dim = z_dim
+        self.conv1 = nn.Conv2d(nc, 64, kernel_size=3, stride=2, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)  # TODO: Remove batch norm?
+        self.layer1 = self._make_layer(BasicBlockEnc, 64, num_Blocks[0], stride=1)
+        self.layer2 = self._make_layer(BasicBlockEnc, 128, num_Blocks[1], stride=2)
+        self.layer3 = self._make_layer(BasicBlockEnc, 256, num_Blocks[2], stride=2)
+        self.layer4 = self._make_layer(BasicBlockEnc, 512, num_Blocks[3], stride=2)
+        self.linear = nn.Linear(512, 2 * z_dim)
+
+    def _make_layer(self, BasicBlockEnc, planes, num_Blocks, stride):
+        strides = [stride] + [1]*(num_Blocks-1)
+        layers = []
+        for stride in strides:
+            layers += [BasicBlockEnc(self.in_planes, stride)]
+            self.in_planes = planes
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = torch.relu(self.bn1(self.conv1(x)))
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = F.adaptive_avg_pool2d(x, 1)
+        x = x.view(x.size(0), -1)
+        x = self.linear(x)
+        mu = x[:, :self.z_dim]
+        logvar = x[:, self.z_dim:]
+        return mu, logvar
+
+class ResNet18Dec(nn.Module):
+
+    def __init__(self, num_Blocks=[2,2,2,2], z_dim=10, nc=3):
+        super().__init__()
+        self.in_planes = 512
+
+        self.linear = nn.Linear(z_dim, 512)
+        self.linear2 = nn.Linear(512, 20*20*512)
+        self.layer4 = self._make_layer(BasicBlockDec, 256, num_Blocks[3], stride=2)
+        self.layer3 = self._make_layer(BasicBlockDec, 128, num_Blocks[2], stride=2)
+        self.layer2 = self._make_layer(BasicBlockDec, 64, num_Blocks[1], stride=2)
+        self.layer1 = self._make_layer(BasicBlockDec, 64, num_Blocks[0], stride=1)
+        # self.lrelu = nn.LeakyReLU(0.1, inplace=True)
+        # self.layer0 = nn.Conv2d(64, 100, 3, 1, 1, bias=True)
+        self.conv1 = ResizeConv2d(64, nc, kernel_size=3, scale_factor=2)
+        # SR - extra upsample
+
+    def _make_layer(self, BasicBlockDec, planes, num_Blocks, stride):
+        strides = [stride] + [1]*(num_Blocks-1)
+        layers = []
+        for stride in reversed(strides):
+            layers += [BasicBlockDec(self.in_planes, stride)]
+        self.in_planes = planes
+        return nn.Sequential(*layers)
+
+    def forward(self, z):
+        x = self.linear(z)
+        x = self.linear2(x)
+        x = x.view(z.size(0), 512, 20, 20)
+        # x = F.interpolate(x, scale_factor=2)
+        x = self.layer4(x)
+        x = self.layer3(x)
+        x = self.layer2(x)
+        x = self.layer1(x)
+        # TODO: Remove sigmoid since it makes the output bounded?
+        x = torch.sigmoid(self.conv1(x))
+        x = self.conv1(x)
+        return x
+
+class SRVAE(nn.Module):
+
+    def __init__(self, z_dim, embed_dim, num_feat, n_colors):
+        super().__init__()
+        self.encoder = ResNet18Enc(z_dim=z_dim, nc=64)
+        self.decoder = ResNet18Dec(z_dim=z_dim, nc=64)
+        self.upsample = nn.PixelShuffle(2)
+        # self.upconv1 = nn.Conv2d(embed_dim, num_feat * 4, 3, 1, 1, bias=True)
+        # self.upconv2 = nn.Conv2d(num_feat, n_colors * 4, 3, 1, 1, bias=True)
+        self.upconv3 = nn.Conv2d(64, n_colors * 4, 3, 1, 1, bias=True)
+        self.lrelu = nn.LeakyReLU(0.1, inplace=True)
+    
+    def forward(self, x):
+        self.mu, self.log_var = self.encoder(x)
+        z = self.reparameterize(self.mu, self.log_var)
+        x = self.decoder(z)
+        # x = self.lrelu(self.upsample(self.upconv1(x)))
+        x = self.upsample(self.upconv3(x))
+        # x = self.upsample(self.upconv2(x))
+        return x, self.mu, self.log_var
+    
+    @staticmethod
+    def reparameterize(mu, log_var):
+        std = torch.exp(log_var / 2) # in log-space, squareroot is divide by two
+        epsilon = torch.randn_like(std)
+        return epsilon * std + mu
 
 class BSRT(nn.Module):
     def __init__(self, args, nframes=8, img_size=64, patch_size=1, in_chans=3, out_chans=3,
@@ -446,6 +614,11 @@ class BSRT(nn.Module):
         self.lrelu = nn.LeakyReLU(0.1, inplace=True)
         self.lrelu2 = nn.LeakyReLU(0.1, inplace=True)
 
+        #####################################################################################################
+        ################################### 6, ResNet-18 VAE ################################################
+        # TODO: Get as arg from somewhere
+        z_dim = 10
+        self.vae_reconstructor = SRVAE(z_dim, num_feat*4, num_feat, args.n_colors)
         self.apply(self._init_weights)
 
 
@@ -521,6 +694,9 @@ class BSRT(nn.Module):
         skip1 = self.lrelu2(self.skip_pixel_shuffle(self.skipup1(self.skip_pixel_shuffle(x_center))))
         skip2 = self.skip_pixel_shuffle(self.skipup2(skip1))
 
+        # inp_rgb = x[:, 0, [0, 1, 3], :, :].contiguous()
+        # inp_rgb_big = F.interpolate(inp_rgb.view(B, 3, H, W), scale_factor=8, mode='bicubic', align_corners=False)
+        # inp_rgb_big = inp_rgb_big.view(B, -1, H*8, W*8)
         x_ = self.conv_flow(self.flow_ps(x.view(B*N, C, H, W))).view(B, N, -1, H*2, W*2)
         
         # calculate flows
@@ -580,14 +756,22 @@ class BSRT(nn.Module):
 
         x = self.lrelu(self.conv_after_body(self.forward_features(x))) + x
 
+        # # Reconstruction - Swin
+        # x = self.lrelu(self.pixel_shuffle(self.upconv1(x)))
+        # x = skip1 + x
+        # x = self.lrelu(self.pixel_shuffle(self.upconv2(x)))
+        # x = self.lrelu(self.HRconv(x))
+        # x = self.conv_last(x)
+        
+        # Reconstruction - VAE
         x = self.lrelu(self.pixel_shuffle(self.upconv1(x)))
         x = skip1 + x
-        x = self.lrelu(self.pixel_shuffle(self.upconv2(x)))
-        x = self.lrelu(self.HRconv(x))
-        x = self.conv_last(x)
-
-        x = skip2 + x
-        return x
+        res, mu, log_var = self.vae_reconstructor(x)
+        x = skip2 + res
+        # x = inp_rgb_big + res
+        # x = res
+        # return x, mu, log_var, skip2, res
+        return x, mu, log_var
 
 
     def get_ref_flows(self, x):
@@ -603,10 +787,3 @@ class BSRT(nn.Module):
                           zip(flows, range(3))]
 
         return flows_list
-
-
-
-
-
-
-
