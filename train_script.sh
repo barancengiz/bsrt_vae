7319b527ea6f82374f840cbb6db9eecb7e6e7a6b
baran <baran.cengiz@kuartis.com>
2023-05-25 23:26:32 +0300

    Add train command

---
+++ train_script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python main.py --n_GPUs 8 --print_every 20 --lr 0.00005 --decay 40-80 --save bsrt_tiny --model BSRT --fp16 --model_level S --swinfeature --batch_size 1 --burst_size 14 --patch_size 80 --pre_train /workspace/bsrt_vae/BSRT_pretrained_models/realworld_models/bsrt_small_realworld.pt
